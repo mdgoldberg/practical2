3d8fb36b5c5b9f291d78104134f76c3e52c2f318
nlarusstone <nlarusstone@college.harvard.edu>
2015-03-05 18:13:38 -0500

    Trying to pull new correctness algorithm

--- learn-predict.py
+++ learn-predict.py
@@ -56,9 +56,9 @@ def main():
     
     # TODO train here, and learn your classification parameters
     print "learning..."
-    rbf_feature = RBFSampler(gamma=0.00075, random_state=1000)
-    X_features = rbf_feature.fit_transform(X_train)
-    #X_features = X_train
+    #rbf_feature = RBFSampler(gamma=0.00075, random_state=1000)
+    #X_features = rbf_feature.fit_transform(X_train)
+    X_features = X_train
     #model = lm.LogisticRegression(penalty='l1', dual=False, tol=1.0, C=100000.0, fit_intercept=True, intercept_scaling=1, class_weight=None, random_state=None)
     #model = lm.BernoulliRBM(n_components=256, learning_rate=0.1, batch_size=10, n_iter=10, verbose=0, random_state=None)
     #model = mix.GMM(n_components=1, covariance_type='diag', random_state=None, thresh=0.01, min_covar=0.001, n_iter=100, n_init=1, params='wmc', init_params='wmc')
@@ -85,7 +85,8 @@ def main():
     
     # TODO make predictions on text data and write them out
     print "making predictions..."
-    X_testfeatures = rbf_feature.transform(X_test)
+    #X_testfeatures = rbf_feature.transform(X_test)
+    X_testfeatures = X_test
     testpreds = model.predict(X_testfeatures)
     #preds = np.argmax(X_test.dot(learned_W),axis=1)
     print "done making predictions"
